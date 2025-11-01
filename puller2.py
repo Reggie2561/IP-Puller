382ebabd6264398c5ad435a316b1c3c506f00f73
Reggie2561 <89669127+Reggie2561@users.noreply.github.com>
2025-10-31 22:16:18 -0400

    Update puller2.py

--- puller2.py
+++ puller2.py
@@ -93,7 +93,7 @@ def update_ips():
                 {"label": "ZIP", "value": safe_get(5)},
                 {"label": "Type", "value": safe_get(7)},
                 {"label": "Username", "value": safe_get(6)},
-                {"label": "Joined Times", "value": safe_get(9)},
+                {"label": "Joined Times", "value": safe_get(8)},
                 {"label": "pps", "value": concurrent_connection.get(ip, {}).get("pps_avg", 0)}
             ],
             "status": status
@@ -685,6 +685,7 @@ def startthread(Target_IP, Target_MAC, Spoof_IP, Spoof_MAC, Routers_MAC, local, 
     sniffer_thread.join()
     conn_thread.join()
     conn_thread2.join()
+
 
 
 
